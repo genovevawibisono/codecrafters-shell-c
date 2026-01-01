a9ef5bab2748e9143f5af8017e3abfc82e19570a
(Oberyn) Genoveva Patricia Wibisono <z5502369@ad.unsw.edu.au>
2026-01-01 11:05:32 +0700

    refactor

--- src/main.c
+++ src/main.c
@@ -1,34 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_COMMAND_LENGTH 1024
 #define DEFAULT_EXIT_STATUS 0
+#define EXIT_LENGTH 4
+#define ECHO_LENGTH 4
 
-void shell_exit(int status);
+struct command_context {
+	bool redirect;
+	char *out_file;
+	char *args;
+};
 
-int main(int argc, char *argv[]) {
-	while (1) {
-		// Flush after every printf
-		setbuf(stdout, NULL);
+typedef void (*command_function)(struct command_context *);
 
-		// Uncomment the code below to pass the first stage
-		fprintf(stdout, "$ ");
+struct command {
+	const char *name;
+	command_function func;
+};
 
-		char command[MAX_COMMAND_LENGTH];
-		fgets(command, MAX_COMMAND_LENGTH, stdin);
-		command[strlen(command) - 1] = '\0';
+void trim_newline(char *s);
+void split_command(char *line, char **cmd, char **args);
+void shell_exit(struct command_context *context);
+void shell_echo(struct command_context *ctx);
 
-		if (strncmp(command,  "exit", strlen(command)) == 0) {
-			shell_exit(DEFAULT_EXIT_STATUS);
-		} else {
-			fprintf(stdout, "%s: command not found\n", command);
-		}
-	}
+struct command commands[] = {
+    { "exit", shell_exit },
+    { "echo", shell_echo },
+};
 
-    return 0;
+#define NUM_COMMANDS (sizeof(commands) / sizeof(commands[0]))
+
+int main(void) {
+    char line[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        setbuf(stdout, NULL);
+        printf("$ ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            break;
+        }
+
+        trim_newline(line);
+
+        char *cmd;
+        char *args;
+        split_command(line, &cmd, &args);
+
+        struct command_context ctx = {
+            .redirect = false,
+            .out_file = NULL,
+            .args = args
+        };
+
+        bool found = false;
+        for (size_t i = 0; i < NUM_COMMANDS; i++) {
+            if (strcmp(cmd, commands[i].name) == 0) {
+                commands[i].func(&ctx);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            printf("%s: command not found\n", cmd);
+        }
+    }
 }
 
-void shell_exit(int status) {
-    exit(status);
+void trim_newline(char *s) {
+    size_t len = strlen(s);
+    if (len > 0 && s[len - 1] == '\n') {
+        s[len - 1] = '\0';
+    }
+}
+
+void split_command(char *line, char **cmd, char **args) {
+    *cmd = line;
+
+    char *space = strchr(line, ' ');
+    if (space) {
+        *space = '\0';
+        *args = space + 1;
+    } else {
+        *args = "";
+    }
+}
+
+void shell_exit(struct command_context *context) {
+	(void) context;
+    exit(DEFAULT_EXIT_STATUS);
+}
+
+void shell_echo(struct command_context *ctx) {
+    fprintf(stdout, "%s\n", ctx->args);
 }

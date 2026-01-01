bc96ebf5b7bdfc80c95abb71da67ac1ead2e6930
(Oberyn) Genoveva Patricia Wibisono <z5502369@ad.unsw.edu.au>
2026-01-01 09:52:38 +0700

    stage 1

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 1024
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
     // TODO: Uncomment the code below to pass the first stage
-    printf("$ ");
+    fprintf(stdout, "$ ");
+
+	char command[MAX_COMMAND_LENGTH];
+	fgets(command, MAX_COMMAND_LENGTH, stdin);
+	command[strlen(command) - 1] = '\0';
+	fprintf(stdout, "%s: command not found\n", command);
 
     return 0;
 }

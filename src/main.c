2c5e5314568811eda4300beb5068cf76f8614bd9
(Oberyn) Genoveva Patricia Wibisono <z5502369@ad.unsw.edu.au>
2026-01-01 10:25:53 +0700

    duct tape exit

--- src/main.c
+++ src/main.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "exit.h"
+
 #define MAX_COMMAND_LENGTH 1024
+#define DEFAULT_EXIT_STATUS 0
 
 int main(int argc, char *argv[]) {
 	while (1) {
 		// Flush after every printf
 		setbuf(stdout, NULL);
 
-		// TODO: Uncomment the code below to pass the first stage
+		// Uncomment the code below to pass the first stage
 		fprintf(stdout, "$ ");
 
 		char command[MAX_COMMAND_LENGTH];
 		fgets(command, MAX_COMMAND_LENGTH, stdin);
 		command[strlen(command) - 1] = '\0';
-		fprintf(stdout, "%s: command not found\n", command);
+
+		if (strncmp(command,  "exit", strlen(command)) == 0) {
+			shell_exit(DEFAULT_EXIT_STATUS);
+		} else {
+			fprintf(stdout, "%s: command not found\n", command);
+		}
 	}
 
     return 0;

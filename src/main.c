1db3b5afafde014dd59af26fb0baf6dcb6fe8a05
(Oberyn) Genoveva Patricia Wibisono <z5502369@ad.unsw.edu.au>
2026-01-01 10:14:36 +0700

    repl

--- src/main.c
+++ src/main.c
@@ -5,16 +5,18 @@
 #define MAX_COMMAND_LENGTH 1024
 
 int main(int argc, char *argv[]) {
-    // Flush after every printf
-    setbuf(stdout, NULL);
+	while (1) {
+		// Flush after every printf
+		setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
-    fprintf(stdout, "$ ");
+		// TODO: Uncomment the code below to pass the first stage
+		fprintf(stdout, "$ ");
 
-	char command[MAX_COMMAND_LENGTH];
-	fgets(command, MAX_COMMAND_LENGTH, stdin);
-	command[strlen(command) - 1] = '\0';
-	fprintf(stdout, "%s: command not found\n", command);
+		char command[MAX_COMMAND_LENGTH];
+		fgets(command, MAX_COMMAND_LENGTH, stdin);
+		command[strlen(command) - 1] = '\0';
+		fprintf(stdout, "%s: command not found\n", command);
+	}
 
     return 0;
 }
